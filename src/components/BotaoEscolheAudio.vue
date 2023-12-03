036119c394e9f6bef9732ef84282f3a57a886de9
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-12-03 20:18:55 -0300

    Finalizando serviço de escolha de aúdios

--- src/components/BotaoEscolheAudio.vue
+++ src/components/BotaoEscolheAudio.vue
@@ -1,7 +1,7 @@
 <script setup>
-    import { onMounted } from 'vue';
+    import { onMounted, reactive } from 'vue';
 
-    var audios = [{nome: 'Default', path: 'default.mp3'}]
+    var audios = reactive([{nome: 'Default', path: 'src/assets/alarm-clock.wav'}])
 
     onMounted(() => {
         selecionaAudio("0")
@@ -22,7 +22,58 @@
 
     const selecionaAudio = (id) => {
         let inputAudio = document.getElementById('input-audio')
+        let inputAudioLabel = document.getElementById('input-audio-label')
+
+        inputAudioLabel.value = audios[Number.parseInt(id)].nome
         inputAudio.value = audios[Number.parseInt(id)].path
+
+    }
+
+    const tocaAudio = () => {
+        let inputAudio = document.getElementById('input-audio')
+        let audioSrc = inputAudio.value
+
+        let audio = new Audio(audioSrc)
+        let durationMax = 3;
+
+        audio.addEventListener('durationchange', function() {
+            if (audio.duration !== Infinity) {
+                if (audio.duration > durationMax) {
+                setTimeout(function() {
+                        audio.pause();
+                    }, durationMax * 1000);
+                }
+            }
+            });
+        audio.play()
+    }
+
+    const importarAudio = () => {
+        document.getElementById('audioPath').click()
+    }
+
+    const pegarAudio = () => {
+        let audioInput = document.getElementById('audioPath')
+
+        const regexTypeAudio = /^audio\/(wav|mp3|mpeg)$/
+        if (regexTypeAudio.test(audioInput.files[0].type)){
+            let audioPath = URL.createObjectURL(audioInput.files[0])
+            console.log(audioPath)
+
+            const objetoAudio = {nome: audioInput.files[0].name, path: audioPath}
+
+            if (audios.indexOf(objetoAudio) === -1){
+                audios.push(objetoAudio)
+            }
+
+            document.getElementById('input-audio-label').value = objetoAudio.nome
+            document.getElementById('input-audio').value = objetoAudio.path
+        }
+        else {
+            alert("Somente é possível utilizar arquivos .wav ou .mp3 ou .mpeg")
+            audioInput.value = ""
+        }
+ 
     }
     
 </script>
@@ -30,13 +81,18 @@
 <template>
     <div>
         <div class="select-audio">
-            <input type="text" id="input-audio" readonly placeholder="Escolha um aúdio" @focus="mostrarOpcoes()" @blur="esconderOpcoes()">
+            <input type="text" id="input-audio-label"  readonly placeholder="Escolha um aúdio" @focus="mostrarOpcoes()" @blur="esconderOpcoes()">
+            <input type="hidden" id="input-audio">
 
             <div v-for="audio in audios" id="opcoes" class="tw-absolute tw-hidden tw-w-20 tw-h-10 tw-bg-slate-500">
-                <div class="opcao" :id="audios.indexOf(audio)" @mousedown="selecionaAudio(audios.indexOf(audio))">{{ audio.nome }}</div>
+                <div class="opcao" :id="audios.indexOf(audio)" :key="audios.indexOf(audio)" @mousedown="selecionaAudio(audios.indexOf(audio))">{{ audio.nome }}</div>
             </div>
         </div>
         <button @click="tocaAudio()">Play</button>
-        <button @click="importarAudio()">Importar Aúdio</button>
+        <div id="importaAudio">
+            <input type="file" id="audioPath" @change="pegarAudio()" accept="audio/*" class="tw-hidden">
+            <button @click="importarAudio()">Importar Aúdio</button>
+        </div>
+        
     </div>
 </template>
