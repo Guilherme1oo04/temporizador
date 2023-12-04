403ced24c3c979f2a55f2ba48a246ba12265cf6b
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-12-04 19:31:21 -0300

    Estilizando botões da função escolher aúdio

--- src/components/BotaoEscolheAudio.vue
+++ src/components/BotaoEscolheAudio.vue
@@ -9,14 +9,14 @@
 
     const mostrarOpcoes = () => {
         let opcoes = document.getElementById('opcoes')
-        opcoes.classList.add('tw-block')
         opcoes.classList.remove('tw-hidden')
+        opcoes.classList.add('tw-block')
     }
 
     const esconderOpcoes = () => {
         let opcoes = document.getElementById('opcoes')
-        opcoes.classList.add('tw-hidden')
         opcoes.classList.remove('tw-block')
+        opcoes.classList.add('tw-hidden')
         
     }
 
@@ -58,7 +58,6 @@
         const regexTypeAudio = /^audio\/(wav|mp3|mpeg)$/
         if (regexTypeAudio.test(audioInput.files[0].type)){
             let audioPath = URL.createObjectURL(audioInput.files[0])
-            console.log(audioPath)
 
             const objetoAudio = {nome: audioInput.files[0].name, path: audioPath}
 
@@ -79,20 +78,30 @@
 </script>
 
 <template>
-    <div>
+    <div class="tw-mt-8 tw-flex tw-flex-col tw-items-center">
         <div class="select-audio">
             <input type="text" id="input-audio-label"  readonly placeholder="Escolha um aúdio" @focus="mostrarOpcoes()" @blur="esconderOpcoes()">
             <input type="hidden" id="input-audio">
 
-            <div v-for="audio in audios" id="opcoes" class="tw-absolute tw-hidden tw-w-20 tw-h-10 tw-bg-slate-500">
-                <div class="opcao" :id="audios.indexOf(audio)" :key="audios.indexOf(audio)" @mousedown="selecionaAudio(audios.indexOf(audio))">{{ audio.nome }}</div>
+            <div class="tw-absolute tw-hidden" id="opcoes">
+                <div v-for="audio in audios" class="tw-bg-slate-400">
+                    <div class="opcao" :id="audios.indexOf(audio)" :key="audios.indexOf(audio)" @mousedown="selecionaAudio(audios.indexOf(audio))"><p>{{ audio.nome }}</p></div>
+                </div>
             </div>
         </div>
-        <button @click="tocaAudio()">Play</button>
-        <div id="importaAudio">
-            <input type="file" id="audioPath" @change="pegarAudio()" accept="audio/*" class="tw-hidden">
-            <button @click="importarAudio()">Importar Aúdio</button>
+        <div class="tw-flex tw-flex-col tw-w-32 tw-h-24 sm:tw-flex-row sm:tw-w-72 tw-justify-around tw-mt-3">
+            <button @click="tocaAudio()" class="button">Play</button>
+            <div id="importaAudio">
+                <input type="file" id="audioPath" @change="pegarAudio()" accept="audio/*" class="tw-hidden">
+                <button @click="importarAudio()" class="button">Import Audio</button>
+            </div>
         </div>
         
     </div>
 </template>
+
+<style>
+    .button {
+        @apply tw-bg-indigo-500 tw-text-neutral-100 tw-font-semibold tw-font-mono tw-text-sm sm:tw-text-lg tw-px-4 tw-py-1 tw-rounded-xl tw-h-10 hover:tw-bg-indigo-700 hover:tw-shadow-lg;
+    }
+</style>
