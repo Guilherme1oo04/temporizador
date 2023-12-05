5db89c52696421dea90f0f1f0d2006d778c45693
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-12-05 19:03:23 -0300

    Estilizando título da página

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -5,7 +5,7 @@
 
 <template>
     <main class="tw-flex tw-flex-col tw-items-center tw-mt-28">
-        <h1 class="pageName">Temporizador online 2025</h1>
+        <h1 class="tw-text-xl tw-font-semibold tw-mx-2 tw-text-center sm:tw-text-3xl">Temporizador online 2025</h1>
         <MarcadorTempo></MarcadorTempo>
         <BotaoEscolheAudio></BotaoEscolheAudio>
     </main>
