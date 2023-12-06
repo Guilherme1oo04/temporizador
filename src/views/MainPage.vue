0a7056976f1ac6922fa24767a5cf8b952ec4dc2e
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-12-06 19:12:09 -0300

    Mudança de nome de arquivo + import de componente no main page

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,12 +1,14 @@
 <script setup>
     import MarcadorTempo from "../components/MarcadorTempo.vue"
     import BotaoEscolheAudio from "../components/BotaoEscolheAudio.vue";
+    import EscolheTempo from "../components/EscolheTempo.vue";
 </script>
 
 <template>
     <main class="tw-flex tw-flex-col tw-items-center tw-mt-28">
         <h1 class="tw-text-xl tw-font-semibold tw-mx-2 tw-text-center sm:tw-text-3xl">Temporizador online 2025</h1>
         <MarcadorTempo></MarcadorTempo>
+        <EscolheTempo></EscolheTempo>
         <BotaoEscolheAudio></BotaoEscolheAudio>
     </main>
 </template>
