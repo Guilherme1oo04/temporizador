708057358cf33706362ba7a7ef4b4db77b5eaa21
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-12-07 19:37:30 -0300

    Estilização do marcador de tempo

--- src/components/MarcadorTempo.vue
+++ src/components/MarcadorTempo.vue
@@ -2,7 +2,16 @@
 </script>
 
 <template>
-    <div>
+    <div id="marcador-tempo" class="tw-my-2 tw-text-3xl sm:tw-text-6xl">
         00:00:00
     </div>
 </template>
+
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700&display=swap');
+
+    #marcador-tempo {
+        font-family: 'Orbitron', sans-serif;
+    }
+</style>
