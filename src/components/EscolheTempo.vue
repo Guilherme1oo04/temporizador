adf293c29bc545c71d0761c5caf2334876f855de
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-12-07 18:58:05 -0300

    Estilização final dos inputs de tempo escolhido

--- src/components/EscolheTempo.vue
+++ src/components/EscolheTempo.vue
@@ -15,10 +15,10 @@
 </script>
 
 <template>
-    <div class="tw-flex tw-w-64 tw-justify-around tw-text-center tw-my-2">
+    <div class="tw-flex tw-w-48 tw-justify-between tw-text-center tw-my-2 sm:tw-w-64">
         <div v-for="tipo in tiposHorarios">
             <input type="number" :id="tipo" min="0" max="99" @input="formataTempo(tipo)" class="input-tempo">
-            <p class="paragrafo-input-tempo tw-pt-2"><label :for="tipo" class="label-input-tempo tw-p-1 tw-rounded-sm">{{ tipo.charAt(0).toUpperCase() + tipo.slice(1) }}</label></p>
+            <p class="paragrafo-input-tempo tw-pt-2 tw-text-xs sm:tw-text-base"><label :for="tipo" class="label-input-tempo tw-p-1 tw-rounded-sm">{{ tipo.charAt(0).toUpperCase() + tipo.slice(1) }}</label></p>
         </div>
         
     </div>
@@ -26,9 +26,9 @@
 
 <style>
     .input-tempo {
-        @apply tw-text-center tw-bg-neutral-50 tw-text-base tw-font-semibold tw-py-2 tw-px-1 tw-rounded-sm hover:tw-cursor-pointer focus:tw-outline-none;
+        @apply tw-text-center tw-bg-neutral-50 tw-text-sm tw-font-semibold tw-py-2 tw-px-1 tw-rounded-sm hover:tw-cursor-pointer focus:tw-outline-none sm:tw-w-20;
     }
     .input-tempo:focus ~ .paragrafo-input-tempo .label-input-tempo {
-        @apply tw-bg-emerald-500 tw-text-neutral-100;
+        @apply tw-bg-indigo-500 tw-text-neutral-100;
     }
 </style>
