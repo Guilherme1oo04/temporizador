b6c7eac89c896345e5b1ff985b270f24b084597b
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-12-07 19:34:46 -0300

    Mudança no layout da página

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -5,8 +5,8 @@
 </script>
 
 <template>
-    <main class="tw-flex tw-flex-col tw-items-center tw-mt-28">
-        <h1 class="tw-text-xl tw-font-semibold tw-mx-2 tw-text-center sm:tw-text-3xl">Temporizador online 2025</h1>
+    <h1 class="tw-text-xl tw-font-semibold tw-mx-2 tw-mt-10 tw-text-center sm:tw-text-3xl">Temporizador online 2025</h1>
+    <main class="tw-flex tw-flex-col tw-items-center tw-mt-12">
         <MarcadorTempo></MarcadorTempo>
         <EscolheTempo></EscolheTempo>
         <BotaoEscolheAudio></BotaoEscolheAudio>
