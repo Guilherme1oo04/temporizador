d60f645da8a092431f9c6f583d341da86be0a983
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-12-10 11:11:33 -0300

    Finalizando funções de start e restart do temporizador

--- src/components/BotoesUtilities.vue
+++ src/components/BotoesUtilities.vue
@@ -1,45 +1,47 @@
 <script setup>
     import { reactive } from 'vue';
 
-    const contagemStatus = reactive({state: false, textLabel: 'Iniciar'})
+    const contagemStatus = reactive({state: false, textLabel: 'Start'})
 
     let tempoTotal = 0
     let timerInterval;
 
-    const statusTemp = (botao, status) => {
+    const statusTemp = (status) => {
 
-        if (contagemStatus.state == false && contagemStatus.textLabel == 'Iniciar'){
-            let horas = document.getElementById('horas').value
-            let minutos = document.getElementById('minutos').value
-            let segundos = document.getElementById('segundos').value
+        if (contagemStatus.state == false && contagemStatus.textLabel == 'Start'){
+            let horas = document.getElementById('horas')
+            let minutos = document.getElementById('minutos')
+            let segundos = document.getElementById('segundos')
 
-            tempoTotal = (horas * 3600) + (minutos * 60) + (segundos * 1)
+            tempoTotal = (horas.value * 3600) + (minutos.value * 60) + (segundos.value * 1)
 
             contagemStatus.state = true
-            contagemStatus.textLabel = 'Parar'
+            stateBotaoIniciar('Stop')
 
-            iniciarTemp(tempoTotal)
+            iniciarTemp()
+            stateInputs(true)
             
         } else {
             if (status){
-                contagemStatus.textLabel = 'Retornar'
+                pararTemp()
+                stateBotaoIniciar('Return')
             } else {
-                contagemStatus.textLabel = 'Parar'
+                iniciarTemp()
+                stateBotaoIniciar('Stop')
             }
             contagemStatus.state = !contagemStatus.state
         }
 
     }
 
-    const iniciarTemp = (tempo) => {
-        let duration = tempo
+    const iniciarTemp = () => {
         let horas, minutos, segundos
         let marcadorTempo = document.getElementById('marcador-tempo')
 
         timerInterval = setInterval(() => {
-            horas = Math.floor(duration / 3600)
-            minutos = Math.floor(duration / 60 - (horas * 60))
-            segundos = Math.floor(duration % 60)
+            horas = Math.floor(tempoTotal / 3600)
+            minutos = Math.floor(tempoTotal / 60 - (horas * 60))
+            segundos = Math.floor(tempoTotal % 60)
 
             horas = horas < 10 ? '0' + horas : horas
             minutos = minutos < 10 ? '0' + minutos : minutos
@@ -47,26 +49,80 @@
 
             marcadorTempo.innerHTML = `${horas}:${minutos}:${segundos}`
 
-            duration -= 1
+            tempoTotal -= 1
 
-            if (duration < 0){
+            if (tempoTotal < 0){
                 clearInterval(timerInterval)
                 marcadorTempo.innerHTML = "00:00:00"
+                tempoTotal = 0
+
                 contagemStatus.state = false
-                contagemStatus.textLabel = 'Iniciar'
+                stateBotaoIniciar('Start')
+
+                stateInputs(false)
             }
         }, 1000)
+    }
+
+    const pararTemp = () => {
+        clearInterval(timerInterval)
+    }
+
+    const reiniciarTemp = () => {
+        let marcadorTempo = document.getElementById('marcador-tempo')
+
+        clearInterval(timerInterval)
+        marcadorTempo.innerHTML = "00:00:00"
+        tempoTotal = 0
+
+        stateBotaoIniciar('Start')
+        stateInputs(false)
+    }
+
+    const stateInputs = (boolean) => {
+        let horasInput = document.getElementById('horas')
+        let minutosInput = document.getElementById('minutos')
+        let segundosInput = document.getElementById('segundos')
+
+        horasInput.disabled = boolean
+        minutosInput.disabled = boolean
+        segundosInput.disabled = boolean
+    }
+
+    const stateBotaoIniciar = (textLabel) => {
+        let botao = document.getElementById('botao-iniciar')
+
+        contagemStatus.textLabel = textLabel
+
+        if (textLabel == 'Start' || textLabel == 'Return'){
+            botao.classList.add('botao-iniciar')
+            botao.classList.remove('botao-parar')
+        } else {
+            botao.classList.add('botao-parar')
+            botao.classList.remove('botao-iniciar')
+        }
+
     }
 </script>
 
 <template>
-    <div>
-        <button id="botao-iniciar" class="tw-bg-blue-500 hover:tw-bg-blue-700" @click="statusTemp(this, contagemStatus.state)">{{ contagemStatus.textLabel }}</button>
+    <div class="tw-flex tw-flex-row tw-w-48 sm:tw-w-64 tw-justify-around">
+        <button id="botao-iniciar" class="botao-iniciar" @click="statusTemp(contagemStatus.state)">{{ contagemStatus.textLabel }}</button>
+        <button id="botao-restart" class="botao-reiniciar" @click="reiniciarTemp()">Restart</button>
     </div>
 </template>
 
 <style>
     #botao-iniciar {
         @apply tw-text-neutral-100 tw-font-semibold tw-font-mono tw-text-sm sm:tw-text-lg tw-px-4 tw-py-1 tw-rounded-xl tw-h-10 hover:tw-shadow-lg;
+    }
+    .botao-iniciar {
+        @apply tw-bg-blue-500 hover:tw-bg-blue-700
+    }
+    .botao-parar {
+        @apply tw-bg-red-500 hover:tw-bg-red-700
+    }
+    .botao-reiniciar {
+        @apply tw-text-neutral-100 tw-font-semibold tw-font-mono tw-text-sm sm:tw-text-lg tw-px-4 tw-py-1 tw-rounded-xl tw-h-10 hover:tw-shadow-lg tw-bg-yellow-500 hover:tw-bg-yellow-700;
     }
 </style>
