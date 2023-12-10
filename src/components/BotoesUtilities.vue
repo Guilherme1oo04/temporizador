f44c0423c991d5aaaf7535d77920fbd693ed1ba5
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-12-10 19:22:03 -0300

    feat: implementando funções de tocar o aúdio

--- src/components/BotoesUtilities.vue
+++ src/components/BotoesUtilities.vue
@@ -6,6 +6,23 @@
     let tempoTotal = 0
     let timerInterval;
 
+    const audioTemporizador = new Audio()
+    audioTemporizador.loop = true
+
+    const iniciarAudio = () => {
+        document.getElementById('modal-parar').classList.remove('tw-hidden')
+        document.getElementById('modal-parar').classList.add('tw-flex')
+
+        audioTemporizador.src = document.getElementById('input-audio').value
+        audioTemporizador.play()
+    }
+    const pararAudio = () => {
+        document.getElementById('modal-parar').classList.remove('tw-flex')
+        document.getElementById('modal-parar').classList.add('tw-hidden')
+
+        audioTemporizador.pause()
+    }
+
     const statusTemp = (status) => {
 
         if (contagemStatus.state == false && contagemStatus.textLabel == 'Start'){
@@ -60,6 +77,8 @@
                 stateBotaoIniciar('Start')
 
                 stateInputs(false)
+
+                iniciarAudio()
             }
         }, 1000)
     }
@@ -75,6 +94,7 @@
         marcadorTempo.innerHTML = "00:00:00"
         tempoTotal = 0
 
+        contagemStatus.state = false
         stateBotaoIniciar('Start')
         stateInputs(false)
     }
@@ -84,6 +104,9 @@
         let minutosInput = document.getElementById('minutos')
         let segundosInput = document.getElementById('segundos')
 
+        let inputAudioLabel = document.getElementById('input-audio-label')
+
+        inputAudioLabel.disabled = boolean
         horasInput.disabled = boolean
         minutosInput.disabled = boolean
         segundosInput.disabled = boolean
@@ -106,6 +129,13 @@
 </script>
 
 <template>
+    <div id="modal-parar" class="tw-absolute tw-bg-slate-500 tw-w-screen tw-h-screen tw-top-0 tw-hidden tw-flex-col tw-justify-center tw-items-center">
+        <div class="tw-bg-neutral-200 tw-flex tw-flex-col tw-justify-around tw-items-center tw-w-48 tw-h-48 tw-rounded-lg tw-shadow-lg sm:tw-w-64 sm:tw-h-64 tw-py-4">
+            <p class="tw-text-center tw-font-semibold tw-text-xl sm:tw-text-3xl">The time is over</p>
+            <p class="tw-text-center tw-text-sm sm:tw-text-base">Click below to end the timer</p>
+            <button class="tw-text-neutral-100 tw-font-semibold tw-font-mono tw-text-sm sm:tw-text-lg tw-px-4 tw-py-1 tw-rounded-xl tw-h-10 hover:tw-shadow-lg tw-bg-blue-500 hover:tw-bg-blue-700" @click="pararAudio()">Close</button>
+        </div>
+    </div>
     <div class="tw-flex tw-flex-row tw-w-48 sm:tw-w-64 tw-justify-around">
         <button id="botao-iniciar" class="botao-iniciar" @click="statusTemp(contagemStatus.state)">{{ contagemStatus.textLabel }}</button>
         <button id="botao-restart" class="botao-reiniciar" @click="reiniciarTemp()">Restart</button>
